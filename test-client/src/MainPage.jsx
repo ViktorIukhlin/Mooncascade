9f7837b8738ebfb1f1d148c2df582f4eafe8f4c9
Viktor Iukhlin <iukhlin.v@gmail.com>
2021-06-06 02:20:32 +0300

    some changes for main file

--- test-client/src/MainPage.jsx
+++ test-client/src/MainPage.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+function getRandomNumber(min, max) {
+    return Math.floor(min - 0.5 + Math.random() * (max - min + 1));
+}
 
 const MainPage = () => {
+    const socket = useRef();
+
+    useEffect(() => {
+        socket.current = new WebSocket("ws://localhost:5000");
+    }, []);
+
+    const start = () => {
+        for (let i = 1; i < 21; i++) {
+            let firstPoint = getRandomNumber(3000, 15000);
+
+            setTimeout(() => {
+                socket.current.send(
+                    JSON.stringify({
+                        identifier: i,
+                        timing_point: "Start",
+                        time: firstPoint,
+                        event: "message",
+                    })
+                );
+
+                let secondPoint = getRandomNumber(5000, 10000);
+                setTimeout(() => {
+                    socket.current.send(
+                        JSON.stringify({
+                            identifier: i,
+                            timing_point: "Finish",
+                            time: secondPoint,
+                            event: "message",
+                        })
+                    );
+                }, secondPoint);
+            }, firstPoint);
+        }
+    };
+
     return (
         <div className="center">
-            <div></div>
+            <div onClick={start}>START</div>
         </div>
     );
 };
